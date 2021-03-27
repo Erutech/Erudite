446b36e80f1f4054177dbf560aea7317a992bae8
Jason <jac23662@utexas.edu>
2021-03-27 15:49:04 -0500

    Added gainxp method and lvlup method
    ^^^

--- app/src/main/java/com/example/erudite/Eruditechi.java
+++ app/src/main/java/com/example/erudite/Eruditechi.java
@@ -1,9 +1,10 @@
 package com.example.erudite;
+import java.lang.Math;
 
 public abstract class Eruditechi {
     int[] evolveLvls = {5, 10};
     String specieName, nickname;
-    int xp, xpMax;
+    int xp, xpMax, lvl;
     String currEvo, evoStages, image;
 
     /**
@@ -70,11 +71,27 @@ public abstract class Eruditechi {
 
     /** extra functions **/
     public void gainxp(int score) {
-        //TODO: set xp and if reach max, lvlup
+        int gainedxp;
+        gainedxp = Math.ceil(score/10);
+        for(int i = 0; i < gainedxp; i++) {
+            xp++;
+            xpMax--;
+            if(xpMax == 0) {
+                lvlup();
+            }
+            
+        }
+
     }
 
-    public void lvlup() {
-        //TODO: set lvl and if reach evo level, evolve
+    public void lvlup() {   
+        lvl++;
+        xpMax = xp + lvl*5;
+        for(int i = 0; i < evolveLvls.length; i++) {
+            if(evolveLvls[i] == lvl) {
+                evolve();
+            }
+        }
     }
 
     public void evolve() {
