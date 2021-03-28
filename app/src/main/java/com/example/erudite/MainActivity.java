aa17229ea276c632a69972101ba09a346b8fe454
Theresa <crawfolivia@gmail.com>
2021-03-28 00:51:13 -0500

    slide up but buggy

--- app/src/main/java/com/example/erudite/MainActivity.java
+++ app/src/main/java/com/example/erudite/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.erudite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     // references to buttons and other controls on the layout
     String true_name, true_abc123, true_courses, true_eruID; //should be changed to variable
 
-    private Button menuBtn;
+    private AppBarConfiguration mAppBarConfiguration;
     private ProgressBar mProgressBar;
 
     // create object for database
@@ -19,8 +29,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
+  /*
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+*/
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_info, R.id.nav_profile)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
+/*
         Erugon myErugon = new Erugon("Bud");
         setProgressBar(myErugon);
 
@@ -39,10 +65,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText( MainActivity.this, "Error creating student", Toast.LENGTH_SHORT).show();
         }
         DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
-        boolean sucess = dataBaseHelper.addOne(studentModel);
-    }
-    public void openMenu() {
-        //Intent intent = new Intent(this, menu)
+        boolean sucess = dataBaseHelper.addOne(studentModel);*/
     }
 
     public void setProgressBar(Erugon myErugon) {
@@ -50,5 +73,12 @@ public class MainActivity extends AppCompatActivity {
         mProgressBar.setMax(myErugon.getXpMax());
         myErugon.setXp(2);
         mProgressBar.setProgress(myErugon.getXp());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/com/example/erudite/ui/home/HomeFragment.java
@@ -0,0 +1,35 @@
+package com.example.erudite.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.erudite.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.activity_main, container, false);
+        final TextView textView = root.findViewById(R.id.textView);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
